14c3acba868102ba5a35e760d3cfbd9ee986b46e
peerasak-hub <78993119+peerasak-hub@users.noreply.github.com>
2021-02-20 15:17:09 +0700

    ใส่รูป

--- src/screen/home/index.js
+++ src/screen/home/index.js
@@ -1,5 +1,92 @@
+import { useState } from "react";
+import {
+  Button,
+  Collapse,
+  Container,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Jumbotron,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
+import React from "react";
+import Slider from "infinite-react-carousel";
 const Home = () => {
-    return <h1>Home Page</h1>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Container>
+        <Jumbotron>
+          <h1> Hello, world! </h1>
+        </Jumbotron>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          </Collapse>
+        </Navbar>
+
+        <Slider dots autoplay>
+          <div>
+            <img src="https://www.cmru.ac.th/assets/images/banners/th/03-02-2021/26c8b10dc51e25adeb818e575d44287c3f15b1b3.png"
+            alt="รูปแรก"
+            />
+          </div>
+          <div>
+          <img 
+            src="https://www.cmru.ac.th/assets/images/banners/th/04-02-2021/5781919f63882117a2c1233753b37f1a36d7ef05.png"
+            alt="รูปสอง"
+            />
+          </div>
+          <div>
+          <img 
+            src="https://www.cmru.ac.th/assets/images/banners/th/06-01-2021/98ffb400b8efa9c2c1ef328aa3ddbbb011049e41.png"
+            alt="รูปสาม"
+            />
+          </div>
+          <div>
+          <img 
+            src="https://www.cmru.ac.th/assets/images/banners/th/29-12-2020/355e723cf01db446a3ad5daa70225bb1901936d1.png"
+            alt="รูปสี่"
+            />
+          </div>
+        </Slider>
+        <h1>Home Page</h1>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
